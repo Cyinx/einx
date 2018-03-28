6df70e96b5e631e3d52e17dd1626b1f60610a2c5
cyinx-home <1494193739@qq.com>
2018-03-28 15:20:07 +0800

    maybe rwqueue is better

--- event/ev_queue.go
+++ event/ev_queue.go
@@ -1,36 +1,7 @@
 package event
 
-/*
-type EventChan chan EventMsg
-type EventQueue struct {
-	ev_queue EventChan
-}
-
-func NewEventQueue() *EventQueue {
-
-	queue := &EventQueue{
-		ev_queue: make(EventChan, 65535 * 2),
-	}
-	return queue
-}
-
-func (this *EventQueue) Push(event EventMsg) {
-	this.ev_queue <- event
-}
-
-func (this *EventQueue) Get() EventMsg {
-	event := <-this.ev_queue
-	return event
-}
-
-func (this *EventQueue) GetChan() EventChan {
-	return this.ev_queue
-}
-
-*/
 import (
 	"github.com/Cyinx/einx/queue"
-	//"sync"
 	"sync/atomic"
 )
 
@@ -48,7 +19,6 @@ func NewEventQueue() *EventQueue {
 		ev_queue: queue.NewRWQueue(),
 		ev_cond:  make(EventChan, 128),
 	}
-	//queue.ev_cond <- true
 	atomic.AddInt32(&queue.wait_count, 1)
 	return queue
 }
@@ -63,7 +33,6 @@ func (this *EventQueue) Push(event EventMsg) {
 		if atomic.CompareAndSwapInt32(&this.wait_count, 0, 0) == true {
 			return
 		}
-
 		old_val := atomic.LoadInt32(&this.wait_count)
 
 		if atomic.CompareAndSwapInt32(&this.wait_count, old_val, old_val-1) == true {
@@ -77,7 +46,6 @@ func (this *EventQueue) Get(event_list []interface{}, count uint32) uint32 {
 	read_count, left_count := this.ev_queue.Get(event_list, count)
 	if left_count == 0 {
 		atomic.AddInt32(&this.wait_count, 1)
-		//atomic.CompareAndSwapInt32(&this.wait_count, 0, 1)
 	} else {
 		this.ev_cond <- true
 	}
